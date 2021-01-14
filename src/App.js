abe6fe8cf74d576ddd0569b45cd8d1883d2a6328
amir rebbah <amir.rebbah16@gmail.com>
2021-01-14 11:33:37 +0100

    part1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+    <div className="titre">
+
+      <h1>exo1</h1>
+
+    </div>
+
+  </div>
+  );
+}
+
+export default App;
